891585d25e70bf097b3d339702426b1eee100394
Shahak Morag <shahak.morag@gmail.com>
2021-11-19 13:39:17 +0200

    allow to read and write from process memory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::ffi::CString;
+use std::ffi::c_void;
 use std::fs;
 use std::error;
 use std::i64;
+use std::mem::size_of;
+use std::os::raw::c_long;
 use nix::sys::ptrace;
 use nix::sys::signal;
 use regex::Regex;
@@ -34,6 +38,38 @@ fn get_process_libc(pid: Pid) -> Result<(u64, String), Box<dyn error::Error>> {
     Err("libc wasn't found")?
 }
 
+fn read_process_memory(pid: Pid, address: u64, length: u64) -> Result<Vec<u8>, Box<dyn error::Error>> {
+    let mut words_from_memory = Vec::<c_long>::new();
+
+    for i in (0..length).step_by(size_of::<c_long>()) {
+        let offset = (i as u64) * (size_of::<c_long>() as u64);
+        let current_address = address + offset;
+
+        words_from_memory.push(ptrace::read(pid, current_address as *mut c_void)?);
+    }
+
+    Ok(words_from_memory.iter().flat_map(|x| -> [u8; size_of::<c_long>()] { x.to_ne_bytes() }).collect())
+}
+
+fn write_process_memory(pid: Pid, address: u64, data: &mut Vec<u8>) -> Result<usize, Box<dyn error::Error>> {
+    while data.len() % size_of::<c_long>() != 0 {
+        data.push(b'\0');
+    }
+
+    let data_length = data.len();
+    let data = unsafe { CString::from_vec_unchecked(data.to_vec()) };
+    let data = data.as_ptr();
+
+    for i in (0..data_length).step_by(size_of::<c_long>()) {
+        let offset = (i as u64) * (size_of::<c_long>() as u64);
+        let current_address = address + offset;
+        let current_data_address = (data as u64) + offset;
+
+        unsafe { ptrace::write(pid, current_address as *mut c_void, current_data_address as *mut c_void) }?;
+    }
+
+    Ok(data_length)
+}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let pid = Pid::from_raw(1704);
@@ -45,6 +81,13 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     ptrace::attach(pid).expect("failed to attach process");
 
     signal::kill(pid, signal::SIGSTOP).expect("failed to stop process");
+
+    let libc_start_code = read_process_memory(pid, libc_address + dlopen_offset, 10)
+        .expect("failed to read process memory");
+
+    for l in libc_start_code.iter() {
+        println!("0x{:x}", l);
+    }
 
     Ok(())
 }
